2f6d33c4e76e107c8aedbb19ff8c2bac856e29a0
masst269 <massonit@oregonstate.edu>
2016-12-07 21:50:03 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,62 +1,79 @@
-var fs = require('fs');
 var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
-var people = require('./people');
+
 var app = express();
+var usersData = require('./users-data');
 var port = process.env.PORT || 3000;
 
-Object.keys(people).forEach(function (person) {
-  if (people[person].age >= 65) {
-    people[person].is65OrOlder = true;
-  }
-});
-
+// Use Handlebars as the view engine for the app.
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars');
 
-// Serve static files from public/.
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Render the index page for the root URL path ('/').
 app.get('/', function (req, res) {
   res.render('index-page');
 });
 
-/*
- * Render the people page for the URL path '/people'.  Pass our people data
- * to Handlebars to use in filling out the page template.
- */
-app.get('/people', function (req, res) {
-  res.render('people-page', {
-    pageTitle: 'Famous People',
-    people: people
+function startPause(){
+app.get('/', function (req, res) {
+  res.render('actions-page');
+});
+if(running == 0){
+    running = 1;
+    increment();
+document.getElementById("start").innerHTML = "Pause";
+document.getElementById("startPause").style.backgroundColor = "red";
+document.getElementById("startPause").style.borderColor = "red";
+}
+else{
+    running = 0;
+document.getElementById("start").innerHTML = "Resume";
+document.getElementById("startPause").style.backgroundColor = "green";
+document.getElementById("startPause").style.borderColor = "green";
+}
+
+};
+
+function find(){
+  app.get('/', function (req, res) {
+    res.render('YouWon-page');
   });
+};
+
+function next(){
+  app.get('/', function (req, res) {
+  res.render('LeaderBoard');
 });
+};
 
-/*
- * Use a dynamic route to render a page for each individual person.  Provide
- * that person's data to Handlebars so it can fill out the template.
- */
-app.get('/people/:person', function (req, res, next) {
-
-  var person = people[req.params.person];
-
-  if (person) {
-
-    res.render('person-page', {
-      pageTitle: person.name,
-      person: person
-    });
-
-  } else {
-
-    // If we don't have info for the requested person, fall through to a 404.
-    next();
-
-  }
-
-});
+function reset(){
+    running = 0;
+    time = 0;
+    document.getElementById("start").innerHTML = "Start";
+    document.getElementById("output").innerHTML = "0:00:00:00";
+    document.getElementById("startPause").style.backgroundColor = "green";
+    document.getElementById("startPause").style.borderColor = "green";
+};
+function increment(){
+    if(running == 1){
+        setTimeout(function(){
+            time++;
+            var mins = Math.floor(time/10/60);
+            var secs = Math.floor(time/10 % 60);
+            var hours = Math.floor(time/10/60/60);
+            var tenths = time % 10;
+            if(mins < 10){
+                mins = "0" + mins;
+            }
+            if(secs < 10){
+                secs = "0" + secs;
+            }
+            document.getElementById("output").innerHTML = hours + ":" + mins + ":" + secs + ":" + tenths + "0";
+            increment();
+        },100)
+    }
+};
 
 // If we didn't find the requested resource, send a 404 error.
 app.get('*', function(req, res) {
